e7887e4b15188259fa9202b6360a640f10c3110f
jillian landin <jillianmlandin@icloud.com>
2022-01-25 20:53:41 -0500

    hw3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,58 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Jillian Landin
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: determining averages with for loops
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    number_inputs = eval(input("how many grades will you enter?"))
+    acc = 0
+    for _ in range(number_inputs):
+        grades = eval(input("enter grade"))
+        acc = acc + grades
+    mean = acc / number_inputs
+    print("Average = ", mean)
 
 
 def tip_jar():
-    pass
+    acc = 0
+    for _ in range(5):
+        tips = eval(input("Enter your tip:"))
+        acc = acc + tips
+    print("Total tips: $", acc)
 
 
 def newton():
-    pass
+    number = eval(input("what number do you want to square root?"))
+    approx = eval(input("how many times should we improve the approximation?"))
+    root = 0
+    for i in range(1, approx):
+        root = (number / i) / 2
+        root += (root / 2)
+    print(root)
 
 
 def sequence():
-    pass
+    terms = eval(input("Enter the number of terms you want:"))
+    for i in range(0, terms):
+        acc = i + (i + 1) % 2
+        print(acc, end=" ")
 
 
 def pi():
-    pass
+    num_terms = eval(input("Enter the number of terms you wish to use for approximation:"))
+    acc = 4
+    for i in range(1, num_terms + 1):
+        num = i + (i + 2) % 2
+        denom = i + (i + 1) % 2
+        acc = acc * (num / denom)
+    print(acc / 2)
 
 
 if __name__ == '__main__':
